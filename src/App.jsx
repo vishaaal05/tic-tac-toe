e4551e89ab4a01ec9f0ed708e8659048cfd0a16a
Vishal Kumar Gupta <vishaaal05@gmail.com>
2024-07-03 15:43:45 +0530

    done

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import TicTacToe from './components/tic-tac-toe';
+
+
+
+function App() {
+  return (
+ <div>
+  <TicTacToe/>
+ </div>
+  )
+}
+
+export default App
